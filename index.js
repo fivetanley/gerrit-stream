992e1e464c8ed75b094e6efed0da70631d179568
Stanley Stuart <dstanley.stuart@gmail.com>
2012-11-03 20:51:28 -0600

    lol typos

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var GerritStream = require9 './src/gerrit_stream' )
+var GerritStream = require( './src/gerrit_stream' )
 module.exports = GerritStream
 module.exports.GerritStream = GerritStream
 module.exports.GerritProcess = require( './src/gerrit_process' )
