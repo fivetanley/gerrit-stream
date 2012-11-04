352ab8879d0519db3d311157504ef942133ab424
Stanley Stuart <dstanley.stuart@gmail.com>
2012-11-03 20:45:34 -0600

    add files to index.js so they will actually load when required!

---
+++ index.js
@@ -0,0 +1,5 @@
+var GerritStream = require9 './src/gerrit_stream' )
+module.exports = GerritStream
+module.exports.GerritStream = GerritStream
+module.exports.GerritProcess = require( './src/gerrit_process' )
+module.exports.JSONStreams = require( './src/gerrit_json_streams' )
